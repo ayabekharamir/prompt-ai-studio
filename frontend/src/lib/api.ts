6beb995c0dd7971904614192f535b2cf3d31e71f
ayabekharam.ir <ayabekharam.ir@gmail.com>
2026-08-27 03:27:23 +0330

    Update api.ts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_API_URL ||
-    "https://prompt-ai-studio-production.up.railway.app",
+    "https://api.ayabekharam.ir",
   headers: {
     "Content-Type": "application/json",
   },
